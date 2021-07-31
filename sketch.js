b52d7c65706133efd772797f5d05fbd218a656a0
ismahkhan <82942120+ismahkhan@users.noreply.github.com>
2021-07-31 14:24:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var backgroundImg
+var snow = []
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var engine , world;
+
+function setup() {
+  createCanvas(1500,700);
+engine = Engine.create();
+world= engine.world;
+Engine.run(engine);
+}
+
+function preload() {
+  backgroundImg = loadImage("snow2.jpg");
+}
+
+function draw() {
+  background(backgroundImg);  
+
+    if( frameCount% 20 === 0){
+    snow.push(new Snow(random(width/2-700, width/2+700),10,10));
+    }
+
+    for(var i = 0; i < snow.length; i++){
+        snow[i].display();
+        
+    }
+}
